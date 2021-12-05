af59b993afa0e02556d7b9095504153d76200c71
Andrew-Garanin <andrey-garanin-00@mail.ru>
2021-12-05 20:42:05 +0300

    Added Logger

--- FileUpdater/Form1.Designer.cs
+++ FileUpdater/Form1.Designer.cs
@@ -30,30 +30,44 @@ namespace FileUpdater
         private void InitializeComponent()
         {
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 39);
+            this.progressBar1.Location = new System.Drawing.Point(12, 53);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(442, 16);
             this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(186, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Updating...";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(466, 90);
+            this.ClientSize = new System.Drawing.Size(466, 97);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Updating Files";
             this.Shown += new System.EventHandler(this.Form1_Shown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- FileUpdater/Form1.cs
+++ FileUpdater/Form1.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,66 +10,11 @@ namespace FileUpdater
 {
     public partial class Form1 : Form
     {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
         public Form1()
         {
             InitializeComponent();
-            
-        }
-
-        private void Updating()
-        {
-            // Поменять на относительный путь
-            IniFile MyIni = new IniFile(@"D:\Projects\.Net Projects\FileUpdater\FileUpdater\FileUpdater\data\Folders.ini");
-
-            String folderToUpdate = MyIni.Read("FolderToUpdate");
-            String folderUpdated = MyIni.Read("UpdatedFolder");
-
-
-            String[] filesToUpdate = Directory.GetFiles(folderToUpdate);
-            String[] filesUpdated = Directory.GetFiles(folderUpdated);
-
-            progressBar1.Maximum = filesUpdated.Length;
-            progressBar1.Minimum = 0;
-            progressBar1.Step = filesUpdated.Length / 100;
-            progressBar1.Style = ProgressBarStyle.Continuous;
-
-            for (int i = 0; i < filesUpdated.Length; i++)
-            {
-                String fileName = Path.GetFileName(filesUpdated[i]);
-
-                if (File.Exists(Path.Combine(folderToUpdate, fileName)))
-                {
-                    String processName = fileName;
-                    var processExists = Process.GetProcesses().Any(p => p.ProcessName == processName);
-                    if (processExists)
-                    {
-                        Process[] workers = Process.GetProcessesByName(fileName);
-                        foreach (Process worker in workers)
-                        {
-                            worker.Kill();
-                            worker.WaitForExit();
-                            worker.Dispose();
-                        }
-                        continue;
-                    }
-
-                    DateTime dt1 = File.GetLastWriteTime(Path.Combine(folderUpdated, fileName));
-                    DateTime dt2 = File.GetLastWriteTime(Path.Combine(folderToUpdate, fileName));
-                    if (dt1 == dt2)
-                        MessageBox.Show("date is teh same!");
-                    else if (dt1.CompareTo(dt2) > 0) // dt1 позже чем dt2
-                    {
-                        MessageBox.Show("date are different!");
-                        var result = File.ReadAllLines(Path.Combine(folderUpdated, fileName)).Select(m => m.Substring(0, Math.Min(38, m.Length)));
-                        File.WriteAllLines(Path.Combine(folderToUpdate, fileName), result);
-                    }
-                }
-                else
-                {
-                    File.Copy(Path.Combine(folderUpdated, fileName), Path.Combine(folderToUpdate, fileName));
-                }
-                progressBar1.Value++;    
-            } 
         }
 
         private async void Form1_Shown(object sender, EventArgs e)
@@ -82,6 +22,71 @@ namespace FileUpdater
             Updating();
             await Task.Delay(1000);
             Close();
+        }
+
+        private void Updating()
+        {
+            IniFile MyIni = new IniFile(@"..\..\data\Folders.ini");
+
+            String folderToUpdate = MyIni.Read("FolderToUpdate");
+            String updatedFolder = MyIni.Read("UpdatedFolder");
+
+            String[] filesToUpdate = Directory.GetFiles(folderToUpdate);
+            String[] updatedFiles = Directory.GetFiles(updatedFolder);
+
+            // Настройка прогресс бара
+            progressBar1.Maximum = updatedFiles.Length;
+            progressBar1.Minimum = 0;
+            progressBar1.Step = updatedFiles.Length / 100;
+            progressBar1.Style = ProgressBarStyle.Continuous;
+
+            if (updatedFiles.Length == 0)
+            {
+                MessageBox.Show("Folder with updated files is empty.",
+                                "Updating files",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                return;
+            }
+
+            for (int i = 0; i < updatedFiles.Length; i++)
+            {
+                String fileName = Path.GetFileName(updatedFiles[i]);
+
+                if (File.Exists(Path.Combine(folderToUpdate, fileName)))
+                {
+                    String processName = Path.GetFileNameWithoutExtension(fileName);
+                    var processExists = Process.GetProcesses().Any(p => p.ProcessName == processName);
+                    if (processExists)
+                    {
+                        Process[] workers = Process.GetProcessesByName(processName);
+                        foreach (Process worker in workers)
+                        {
+                            worker.Kill();
+                            worker.WaitForExit();
+                            worker.Dispose();
+                            Logger.Info("Closed application " + worker);
+                        }
+                        continue;
+                    }
+
+                    DateTime dt1 = File.GetLastWriteTime(Path.Combine(updatedFolder, fileName));
+                    DateTime dt2 = File.GetLastWriteTime(Path.Combine(folderToUpdate, fileName));
+
+                    if (dt1.CompareTo(dt2) > 0) // dt1 позже чем dt2
+                    {
+                        var result = File.ReadAllLines(Path.Combine(updatedFolder, fileName)).Select(m => m.Substring(0, Math.Min(38, m.Length)));
+                        File.WriteAllLines(Path.Combine(folderToUpdate, fileName), result);
+                        Logger.Info("File " + fileName + " is up to date.");
+                    }
+                }
+                else
+                {
+                    File.Copy(Path.Combine(updatedFolder, fileName), Path.Combine(folderToUpdate, fileName));
+                    Logger.Info("File " + fileName + " added to the FolderToUpdate folder.");
+                }
+                progressBar1.Value++;
+            }
         }
     }
 }
